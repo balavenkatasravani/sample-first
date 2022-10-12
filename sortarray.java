04f5dee66b78572d06c01538d52a500b8f73f207
balavenkatasravani <balaee218@gmail.com>
2022-10-12 18:21:49 +0530

    third

--- sortarray.java
+++ sortarray.java
@@ -3,7 +3,7 @@ package HelloWorld;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.stream.Stream;
-
+int i=1;
 public class sortarray {
 	public static void main(String[] args) {
 		Integer arr[]= {12,16,45,11,18,22,22,29,20,45};
